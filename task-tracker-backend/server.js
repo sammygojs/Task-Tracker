907b98d23e06cb1dafb1270ed02c1b376d17ca51
Sammygojs <akoliyasumit@hotmail.com>
2025-04-25 20:51:35 +0100

    Add basic Express server setup

---
+++ task-tracker-backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app; // For testing
